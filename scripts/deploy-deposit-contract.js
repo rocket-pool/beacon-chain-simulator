a97a4ef9fe18f13d6d043fe259d15fbccc4217a6
Jake Pospischil <moles1@gmail.com>
2019-01-16 11:23:39 +1000

    added deploy deposit contract script CLI options

--- scripts/deploy-deposit-contract.js
+++ scripts/deploy-deposit-contract.js
@@ -1,30 +1,48 @@
 const fs = require('fs');
+const cmd = require('commander');
 const Web3 = require('web3');
 
-// Get contract data
-let depositContractABI = JSON.parse(fs.readFileSync(__dirname + '/../contracts/casper/Deposit.abi'));
-let depositContractBinary = fs.readFileSync(__dirname + '/../contracts/casper/Deposit.bin');
+// Default web3 host
+const defaultWeb3Host = 'http://127.0.0.1:8545';
 
-// Initialise web3
-let web3 = new Web3('http://127.0.0.1:8545');
+// Get contract data
+const depositContractABI = JSON.parse(fs.readFileSync(__dirname + '/../contracts/casper/Deposit.abi'));
+const depositContractBinary = fs.readFileSync(__dirname + '/../contracts/casper/Deposit.bin');
+
+// Initialise CLI
+cmd
+    .option('-h, --host <address>', 'The address of the web3 host')
+    .option('-f, --from <address>', 'The address to deploy the Casper Deposit contract from')
+    .parse(process.argv);
 
 // Deploy deposit contract
 async function deployDepositContract() {
+    try {
 
-    // Get accounts
-    let accounts = await web3.eth.getAccounts();
+        // Process CLI arguments
+        if (!cmd.host) { cmd.host = defaultWeb3Host; }
+        if (!cmd.from) { throw new Error('You must specify a from address (-f, --from).'); }
+        if (!cmd.from.match(/^(0x)?[0-9a-f]{40}$/i)) { throw new Error('Invalid from address.'); }
 
-    // Initialise contract
-    let depositContract = new web3.eth.Contract(depositContractABI);
+        // Initialise web3
+        let web3 = new Web3(cmd.host);
 
-    // Deploy contract
-    let depositContractInstance = await depositContract.deploy({data: depositContractBinary}).send({
-        from: accounts[0],
-        gas: 8000000,
-    });
+        // Initialise contract
+        let depositContract = new web3.eth.Contract(depositContractABI);
 
-    console.log('Casper Deposit contract deployed successfully.');
-    console.log('Casper Deposit contract address: ' + depositContractInstance.options.address);
+        // Deploy contract
+        let depositContractInstance = await depositContract.deploy({data: depositContractBinary}).send({
+            from: cmd.from,
+            gas: 8000000,
+        });
 
+        // Log
+        console.log('Casper Deposit contract deployed successfully.');
+        console.log('Casper Deposit contract address: ' + depositContractInstance.options.address);
+
+    }
+    catch (e) {
+        console.log(e.message);
+    }
 }
 deployDepositContract();
