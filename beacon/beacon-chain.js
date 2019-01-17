c1b9bb150e88c167555d2f3ec5dda8924df79dbf
Jake Pospischil <moles1@gmail.com>
2019-01-17 10:38:54 +1000

    implementing beacon state transitions

--- beacon/beacon-chain.js
+++ beacon/beacon-chain.js
@@ -1,5 +1,12 @@
-// Config
+// Initial values
+const GENESIS_SLOT = 0;
 const FAR_FUTURE_SLOT = Math.pow(2, 64) - 1;
+
+// Time parameters
+const SLOT_DURATION = 3000; // ms
+const EPOCH_LENGTH = 3; // Slots
+const ENTRY_EXIT_DELAY = 1; // Slots
+const MIN_VALIDATOR_WITHDRAWAL_TIME = 1; // Slots
 
 
 /**
@@ -13,7 +20,11 @@ class BeaconChain {
      */
     constructor() {
 
-        // Validator data
+        // Beacon state
+        this.slot = GENESIS_SLOT;
+        this.genesisTime = 0;
+
+        // Validator registry
         this.validatorRegistry = [];
         this.validatorBalances = [];
 
@@ -32,37 +43,61 @@ class BeaconChain {
             this.processDeposit(...args);
         });
 
+        // Start beacon chain processing
+        this.start();
+
     }
 
 
     /**
-     * Process a PoW chain deposit
-     * @param pubkey The validator's public key
-     * @param amount The validator's deposit amount in gwei
-     * @param proofOfPossession The validator's proof of possession
-     * @param withdrawalCredentials The validator's withdrawal credentials
-     * @param randaoCommitment The validator's randao commitment
-     * @param custodyCommitment The validator's custody commitment
+     * =======================
+     * Beacon state management
+     * =======================
      */
-    processDeposit(pubkey, amount, proofOfPossession, withdrawalCredentials, randaoCommitment, custodyCommitment) {
 
-        // Validate proof of possession
-        // TODO: validate once implemented
 
-        // Check for existing validator
-        let index = this.getValidatorIndex(pubkey);
+    /**
+     * Start beacon chain processing
+     */
+    start() {
 
-        // Add validator if it doesn't exist
-        if (index == -1) {
-            this.addValidator(pubkey, amount, withdrawalCredentials, randaoCommitment, custodyCommitment);
-        }
+        // Set genesis time
+        this.genesisTime = Date.now();
 
-        // Increase balance if validator exists
-        else {
-            this.validatorBalances[index] += amount;
-        }
+        // Start slot processing
+        this.slotTimer = setInterval(() => { this.processSlot(); }, SLOT_DURATION);
+        this.processSlot();
 
     }
+
+
+    /**
+     * Per-slot processing
+     */
+    processSlot() {
+
+        // Set slot index
+        this.slot = Math.floor((Date.now() - this.genesisTime) / SLOT_DURATION);
+
+        // Process epochs
+        if (this.slot % EPOCH_LENGTH == 0) this.processEpoch();
+
+    }
+
+
+    /**
+     * Per-epoch processing
+     */
+    processEpoch() {
+
+    }
+
+
+    /**
+     * ====================
+     * Validator management
+     * ====================
+     */
 
 
     /**
@@ -96,6 +131,36 @@ class BeaconChain {
             penalizedSlot: FAR_FUTURE_SLOT,
         });
         return this.validatorBalances.push(amount) - 1;
+    }
+
+
+    /**
+     * Process a PoW chain deposit
+     * @param pubkey The validator's public key
+     * @param amount The validator's deposit amount in gwei
+     * @param proofOfPossession The validator's proof of possession
+     * @param withdrawalCredentials The validator's withdrawal credentials
+     * @param randaoCommitment The validator's randao commitment
+     * @param custodyCommitment The validator's custody commitment
+     */
+    processDeposit(pubkey, amount, proofOfPossession, withdrawalCredentials, randaoCommitment, custodyCommitment) {
+
+        // Validate proof of possession
+        // TODO: validate once implemented
+
+        // Check for existing validator
+        let index = this.getValidatorIndex(pubkey);
+
+        // Add validator if it doesn't exist
+        if (index == -1) {
+            this.addValidator(pubkey, amount, withdrawalCredentials, randaoCommitment, custodyCommitment);
+        }
+
+        // Increase balance if validator exists
+        else {
+            this.validatorBalances[index] += amount;
+        }
+
     }
 
 
